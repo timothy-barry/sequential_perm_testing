2ee177e3bf83cf9c735335bbaf28c91b088f153c
timothy-barry <tbarry2@andrew.cmu.edu>
2024-10-02 16:28:56 -0400

    fix bug in run_all

--- run_all.sh
+++ run_all.sh
@@ -1,10 +1,10 @@
+#!/bin/bash
 #SBATCH -c 1
 #SBATCH -p shared
 #SBATCH --time=0-1:00:00
 #SBATCH --mem=4GB
 #SBATCH -o %j.out
 #SBATCH -e %j.err
-
 Rscript download_and_process_data.R 
 # bash submit_fast_rna_seq_analysis.sh
 # bash submit_classical_rna_seq_analysis.sh
