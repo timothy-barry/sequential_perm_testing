059563ef66ae8c5fad04036df89ed2a1f5781360
Tim Barry <tbarry2@andrew.cmu.edu>
2026-03-04 16:04:06 -0500

    update partition name

--- run_all.sh
+++ run_all.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -c 1
-#SBATCH -p shared
+#SBATCH -p short
 #SBATCH --time=0-1:00:00
 #SBATCH --mem=4GB
 #SBATCH -o %j.out
 #SBATCH -e %j.err
-Rscript ./download_and_process_data.R 
+Rscript ./download_and_process_data.R
 sbatch --wait submit_fast_rna_seq_analysis.sh
 sbatch --wait submit_classical_rna_seq_analysis.sh
 Rscript ./analyze_results.R
